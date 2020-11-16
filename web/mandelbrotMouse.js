8b7cea48cd54b7f85602ae2572bde535e5710b1b
Barnaby Dellar <barnaby.dellar@eu.medical.canon>
2020-11-16 08:28:01 +0000

    Alerting

--- web/mandelbrotMouse.js
+++ web/mandelbrotMouse.js
@@ -98,60 +98,38 @@ function doKeyDown(e) {
 }
 
 function touch_start_handler(ev) {
-    // If the user makes simultaneous touches, the browser will fire a 
-    // separate touchstart event for each touch point. Thus if there are 
-    // three simultaneous touches, the first touchstart event will have 
-    // targetTouches length of one, the second event will have a length 
-    // of two, and so on.
     ev.preventDefault();
-    // Cache the touch points for later processing of 2-touch pinch/zoom
     if (ev.targetTouches.length == 2) {
-        alert("Pinch!");
         for (var i = 0; i < ev.targetTouches.length; i++) {
             tpCache.push(ev.targetTouches[i]);
         }
     }
 }
 function touch_end_handler(ev) {
-    // Note: if the user makes more than one "simultaneous" touches, most browsers 
-    // fire at least one touchmove event and some will fire several touchmoves.
-    // Consequently, an application might want to "ignore" some touchmoves.
-    //
-    // This function sets the target element's border to "dashed" to visually
-    // indicate the target received a move event.
-    //
     ev.preventDefault();
-    // To avoid too much color flashing many touchmove events are started,
-    // don't update the background if two touch points are active
-    if (!(ev.touches.length == 2 && ev.targetTouches.length == 2)) {
-
-        if (ev.targetTouches.length == 2 && ev.changedTouches.length == 2) {
-            // Check if the two target touches are the same ones that started
-            // the 2-touch
-            var point1 = -1, point2 = -1;
-            for (var i = 0; i < tpCache.length; i++) {
-                if (tpCache[i].identifier == ev.targetTouches[0].identifier) point1 = i;
-                if (tpCache[i].identifier == ev.targetTouches[1].identifier) point2 = i;
-            }
-            if (point1 >= 0 && point2 >= 0) {
-                // Calculate the difference between the start and move coordinates
-                var initial_gap = Math.abs(tpCache[point2].clientY - tpCache[point1].clientY);
-                var current_gap = Math.abs(ev.targetTouches[0].clientY - ev.targetTouches[1].clientY);
-                var diff = current_gap - initial_gap;
+    if (ev.targetTouches.length == 2) {
+        var point1 = -1, point2 = -1;
+        for (var i = 0; i < tpCache.length; i++) {
+            if (tpCache[i].identifier == ev.targetTouches[0].identifier) point1 = i;
+            if (tpCache[i].identifier == ev.targetTouches[1].identifier) point2 = i;
+        }
+        if (point1 >= 0 && point2 >= 0) {
+            // Calculate the difference between the start and move coordinates
+            var initial_gap = Math.abs(tpCache[point2].clientY - tpCache[point1].clientY);
+            var current_gap = Math.abs(ev.targetTouches[0].clientY - ev.targetTouches[1].clientY);
+            var diff = current_gap - initial_gap;
 
 
-                // This threshold is device dependent as well as application specific
-                var PINCH_THRESHHOLD = ev.target.clientWidth / 10;
-                if (Math.abs(diff) >= PINCH_THRESHHOLD) {
-                    scale *= -diff;
-                    one_over_min_half = 1 / (scale * Math.min(half_w, half_h));
-                    NewMandelbrot();
-                }
+            // This threshold is device dependent as well as application specific
+            var PINCH_THRESHHOLD = ev.target.clientWidth / 10;
+            if (Math.abs(diff) >= PINCH_THRESHHOLD) {
+                scale *= -diff;
+                one_over_min_half = 1 / (scale * Math.min(half_w, half_h));
+                NewMandelbrot();
             }
-            else {
-                // empty tpCache
-                tpCache = new Array();
-            }
+        }
+        else {
+            tpCache = new Array();
         }
     }
 }
